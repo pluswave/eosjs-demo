8da2804b93c2d663c9f89de17eace7841a5c95ea
Pluswave <pluswave@xiaofuxing.name>
2018-06-26 22:32:44 +0800

    createAccount: clean code

--- createAccount.js
+++ createAccount.js
@@ -2,36 +2,41 @@
 
 const eosInstance = require('./eos-instance');
 
+const creator = 'pluswavemagi';
+const accountName = 'amagicwallet';
+const owner_key = 'EOS5kdTT2HrgU8RmfSUqdXjbzfaiSF4xEeDFrwtA1XTEGbv8Hv6y3'
+const active_key = 'EOS5kdTT2HrgU8RmfSUqdXjbzfaiSF4xEeDFrwtA1XTEGbv8Hv6y3'
+
 eosInstance.transaction(tr => {
     tr.newaccount({
-        creator: 'pluswavemagi',
-        name: 'amagicwallet',
+        creator: creator,
+        name: accountName,
         owner: {
             "threshold": 1,
             "keys": [{
-                "key": "EOS5kdTT2HrgU8RmfSUqdXjbzfaiSF4xEeDFrwtA1XTEGbv8Hv6y3",
+                "key": owner_pubkey,
                 "weight": 1
               }]
         },
         active: {             
             "threshold": 1,
             "keys": [{
-            "key": "EOS5kdTT2HrgU8RmfSUqdXjbzfaiSF4xEeDFrwtA1XTEGbv8Hv6y3",
+            "key": active_pubkey,
             "weight": 1
           }]
         }
     })
    tr.buyrambytes({
-    payer: 'pluswavemagi',
-    receiver: 'amagicwallet',
+    payer: creator,
+    receiver: accountName,
     bytes: 8192
    })
 
   tr.delegatebw({
-    from: 'pluswavemagi',
-    receiver: 'amagicwallet',
-    stake_net_quantity: '10.0000 EOS',
-    stake_cpu_quantity: '10.0000 EOS',
+    from: creator,
+    receiver: accountName,
+    stake_net_quantity: '0.1000 EOS',
+    stake_cpu_quantity: '0.1000 EOS',
     transfer: 0
   })
 }).then(console.log)
